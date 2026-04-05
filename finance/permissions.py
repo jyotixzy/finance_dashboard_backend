b30d48bab03ed8a99d0cb5e0238769b822097155
jyotixzy <kumari008jyoti@gmail.com>
2026-04-05 13:06:22 -0700

    Update permissions.py

--- finance/permissions.py
+++ finance/permissions.py
@@ -5,23 +5,22 @@ class RoleBasedPermission(BasePermission):
     def has_permission(self, request, view):
         user = request.user
 
-        # agar user login nahi hai
         if not user.is_authenticated:
             return False
 
-        # GET request → sab roles allowed
+     
         if request.method in ['GET']:
             return True
 
-        # Viewer → sirf read
+      
         if user.role == 'viewer':
             return False
 
-        # Analyst → read only (no create/update/delete)
+
         if user.role == 'analyst':
             return False
 
-        # Admin → full access
+     
         if user.role == 'admin':
             return True
 
